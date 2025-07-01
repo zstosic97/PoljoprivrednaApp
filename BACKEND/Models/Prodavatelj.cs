5e56246580ee4fa0ec016f4e35beab4e0a1b214b
Marta Došen <stosic.zlatko@gmail.com>
2025-07-01 18:22:36 +0200

    Model klase

---
+++ BACKEND/Models/Prodavatelj.cs
@@ -0,0 +1,10 @@
+﻿namespace BACKEND.Models
+{
+    public class Prodavatelj : Entitet
+    {
+        public string Naziv { get; set; } = ""; 
+        public string AdresaSjedista { get; set; }
+        public string   Email   { get; set; } 
+        
+    }
+}
