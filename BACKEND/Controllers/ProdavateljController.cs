8a28ecc0e027752fca74c2200a883343dc964109
zstosic97 <stosic.zlatko@gmail.com>
2025-07-06 22:50:35 +0200

    Dodali Put i Delete

--- BACKEND/Controllers/ProdavateljController.cs
+++ BACKEND/Controllers/ProdavateljController.cs
@@ -4,12 +4,12 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BACKEND.Controllers
-{ 
+{
     [ApiController]
     [Route("api/v1/[controller]")]
     public class ProdavateljController : ControllerBase
-       
-        
+
+
     {
         // koristimo dependency injection
 
@@ -36,7 +36,7 @@ namespace BACKEND.Controllers
             }
 
         }
-        [HttpPost]        
+        [HttpPost]
         public IActionResult Post(Prodavatelj prodavatelj)
         {
             try
@@ -53,9 +53,77 @@ namespace BACKEND.Controllers
 
         }
 
+        [HttpPut("sifra:int")]
+        public IActionResult Put(int sifra, Prodavatelj prodavatelj)
+        {
+            if (sifra < 1)
+            {
+                return BadRequest(new { poruka = "Šifra mora biti veća od 0" });
+            }
+
+            try
+            {
+                Prodavatelj s = _context.Prodavatelji.Find(sifra);
+
+                if (s == null)
+                {
+                    return NotFound();
+
+                }
+
+                // za sada ručno, klasnije automatika
+                s.Naziv = prodavatelj.Naziv;
+                s.AdresaSjedista = prodavatelj.AdresaSjedista;
+                s.Email = prodavatelj.Email;
+
+                _context.Prodavatelji.Update(s);
+                _context.SaveChanges();
+                return Ok(s);
 
 
 
+
+
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+
+            }
+
+        }
+
+        [HttpDelete("sifra:int")]
+        public IActionResult Delete (int sifra)
+        {
+            if (sifra < 1)
+            {
+                return BadRequest(new { poruka = "Šifra mora biti veća od 0" });
+            }
+
+            try
+            {
+                Prodavatelj s = _context.Prodavatelji.Find(sifra);
+
+                if (s == null)
+                {
+                    return NotFound();
+
+                }
+
+                _context.Prodavatelji.Remove(s);
+                _context.SaveChanges();
+                return NoContent();
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+
+            }
+
+        }
 
        
     }
--- BACKEND/Program.cs
+++ BACKEND/Program.cs
@@ -1,3 +1,4 @@
+
 using BACKEND.Data;
 using Microsoft.EntityFrameworkCore;
 
